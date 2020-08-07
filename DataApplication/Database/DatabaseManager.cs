45654b4913576aa925c405f2f21ca29496f24179
microsoft.all.azure <microsoft.all.azure@gmail.com>
2020-08-07 22:53:54 +0530

    Discount Feature

--- DataApplication/Database/DatabaseManager.cs
+++ DataApplication/Database/DatabaseManager.cs
@@ -3,6 +3,7 @@ using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 
 namespace DataApplication.Database
 {
@@ -304,6 +305,98 @@ namespace DataApplication.Database
                 _connection.Close();
             }
             return result;
+        }
+
+        public int GetCustomerTypeFromCustomer(int CustomerId)
+        {
+            IList<Customer> items = null;
+            Customer customer = null;
+            int customerType = -1;
+            _connection.Open();
+            var query = $"SELECT * FROM `{_customerTableName}` where CustomerId = {CustomerId}";
+            try
+            {
+                using (var command = new MySqlCommand(query, _connection))
+                {
+                    var da = new MySqlDataAdapter(command);
+                    var dt = new DataTable();
+                    da.Fill(dt);
+                    items = dt.ConvertDataTable<Customer>();
+                    customer = items.First();
+                    customerType = customer.Category;
+                }
+            }
+            catch (MySqlException)
+            {
+                Console.WriteLine("Database Conenction Error!");
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return customerType;
+        }
+
+        public string GetCustomerTypeName(int CustomerTypeId)
+        {
+            IList<CustomerType> items = null;
+            CustomerType customerType = null;
+            string customerTypeName = string.Empty;
+            _connection.Open();
+            var query = $"SELECT * FROM `{_customerTypeTableName}` where CustomerTypeId = {CustomerTypeId}";
+            try
+            {
+                using (var command = new MySqlCommand(query, _connection))
+                {
+                    var da = new MySqlDataAdapter(command);
+                    var dt = new DataTable();
+                    da.Fill(dt);
+                    items = dt.ConvertDataTable<CustomerType>();
+                    customerType = items.First();
+                    customerTypeName = customerType.CustomerTypeName;
+                }
+            }
+            catch (MySqlException)
+            {
+                Console.WriteLine("Database Conenction Error!");
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return customerTypeName;
+        }
+
+        public int GetCustomerBookingsCountWithinYear(int customerId)
+        {
+            var year = $"SELECT EXTRACT(YEAR FROM CURRENT_DATE)";
+            IList<Booking> items = null;
+            int count = 0;
+            _connection.Open();
+            var query = $"SELECT * FROM `{_bookingTableName}` where CustId = {customerId} and YEAR(Time) = {year}";
+            try
+            {
+                using (var command = new MySqlCommand(query, _connection))
+                {
+                    var da = new MySqlDataAdapter(command);
+                    var dt = new DataTable();
+                    da.Fill(dt);
+                    items = dt.ConvertDataTable<Booking>();
+                    count = items.Count;
+                }
+            }
+            catch (MySqlException)
+            {
+                Console.WriteLine("Database Conenction Error!");
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return count;
         }
     }
 }
